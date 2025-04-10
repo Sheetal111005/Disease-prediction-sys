c5877e128cffecec0c6885442219728a3c656f69
Aryan Bhandari <aryanbhandari4077@gmail.com>
2025-04-10 14:55:05 +0530

    Files rm

--- build/fix.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("dataset/Features.csv")
-
-df = df.drop_duplicates(subset=["Symptom"])  
-
-df.to_csv("dataset/Features.csv", index=False)
-print("✅ Successfully removed duplicate symptoms!")
