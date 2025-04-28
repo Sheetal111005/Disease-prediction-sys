fd2913732656075bfa736b94009675632c2fe508
Aryan Bhandari <aryanbhandari4077@gmail.com>
2025-04-28 22:40:53 +0530

    CHANGES

---
+++ src/frontend/src/pages/Chat.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Layout from '../layout/Layout'
+import ChatBox from '../components/ChatBox';
+import ChatArea from '../components/ChatArea';
+const Chat = () => {
+
+  const [Messages, setMessages] = useState([]);
+  return (
+    <section className='flex flex-col h-full w-[100vw]'>
+      <ChatArea/>
+      {/* <ChatBox/> */}
+    </section>
+    
+  )
+}
+
+export default Layout(Chat);
